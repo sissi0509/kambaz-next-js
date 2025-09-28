07568fee671964f2abcd30d6bdcc6013d8d8bd48
xi zhao <sissi.zhao09@gmail.com>
2025-09-28 12:44:41 -0400

    add lab2

---
+++ app/Labs/Lab2/Float.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+export default function Float() {
+  return (
+    <div id="wd-float-divs">
+      <h2>Float</h2>
+      <div>
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic Lorem
+        ipsum, dolor sit amet consectetur adipisicing elit. Eius hic Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Recusandae odio at
+        laudantium rem dolor quaerat quibusdam blanditiis. Cumque enim rerum
+        ullam, doloremque, eveniet repellendus eligendi adipisci numquam natus
+        laudantium iusto.loremque, eveniet repellendus eligendi adipisci numquam
+        natus laudantium iusto. Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Labore ipsa, fugiat id, nulla minus totam sequi quae
+        est harum corrupti a ea aspernatur facilis excepturi eveniet hic saepe
+        vitae! Culpa!
+      </div>
+      <div>
+        <img
+          className="wd-float-left"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic Lorem
+        ipsum, dolor sit amet consectetur adipisicing elit. Eius hic Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Eligendi quam minima
+        voluptatum veritatis adipisci quasi officia ex tempore at nulla ad
+        excepturi mollitia aspernatur, suscipit possimus odit provident
+        consequatur quidem? Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Eveniet sit ipsam sunt natus adipisci quo! Ullam dignissimos est
+        reprehenderit labore, corrupti ea qui aliquam eos omnis quia, facere
+        nemo vel?
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic Lorem
+        ipsum, dolor sit amet consectetur adipisicing elit. Eius hic Lorem ipsum
+        dolor sit amet consectetur, adipisicing elit. Assumenda, ratione odit
+        repudiandae laboriosam optio harum ipsa. Nobis, labore itaque, obcaecati
+        nisi sit fugit neque ratione aspernatur consequuntur consectetur
+        numquam. Placeat! Lorem ipsum dolor sit amet, consectetur adipisicing
+        elit. Id sit perferendis, officia similique officiis cumque voluptas
+        aperiam illum maxime a eaque aliquid magnam quam voluptatibus ipsum
+        laborum molestiae distinctio iste?
+        <img
+          className="wd-float-left"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic Lorem
+        ipsum, dolor sit amet consectetur adipisicing elit. Eius hic Lorem
+        ipsum, dolor sit amet consectetur adipisicing elit. Sunt pariatur
+        molestias consequuntur nam natus numquam neque quibusdam? Eligendi quas
+        delectus vero soluta eveniet, quos nisi minima tenetur quisquam vitae
+        vel! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam,
+        accusamus fugiat quidem reprehenderit, repellat repellendus dolore
+        laboriosam et atque corporis, consectetur facilis libero labore saepe
+        illum iure cupiditate iste laudantium.
+        <div className="wd-float-done"></div>
+      </div>
+      <div>
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+          Yellow{" "}
+        </div>
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+          Blue{" "}
+        </div>
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+          Red{" "}
+        </div>
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        <div className="wd-float-done"></div>
+      </div>
+    </div>
+  );
+}
