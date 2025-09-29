9a5c4b873adf5f999f85031a737443872d23ea20
xi zhao <sissi.zhao09@gmail.com>
2025-09-28 22:18:44 -0400

    fix modules cid

--- app/(Kambaz)/Courses/[cid]/Navigation.tsx
+++ app/(Kambaz)/Courses/[cid]/Navigation.tsx
@@ -1,36 +1,39 @@
 import Link from "next/link";
-export default function CourseNavigation() {
+export default function CourseNavigation({ cid }: { cid: string }) {
   return (
     <div id="wd-course-navigation">
-      <Link href="/Courses/1234/Home" id="wd-course-home-link">
+      <Link href={`/Courses/${cid}/Home`} id="wd-course-home-link">
         Home
       </Link>
       <br />
-      <Link href="/Courses/1234/Modules" id="wd-course-modules-link">
+      <Link href={`/Courses/${cid}/Modules`} id="wd-course-modules-link">
         Modules
       </Link>
       <br />
-      <Link href="/Courses/1234/Piazza" id="wd-course-piazza-link">
+      <Link href={`/Courses/${cid}/Piazza`} id="wd-course-piazza-link">
         Piazza
       </Link>
       <br />
-      <Link href="/Courses/1234/Zoom" id="wd-course-zoom-link">
+      <Link href={`/Courses/${cid}/Zoom`} id="wd-course-zoom-link">
         Zoom
       </Link>
       <br />
-      <Link href="/Courses/1234/Assignments" id="wd-course-assignments-link">
+      <Link
+        href={`/Courses/${cid}/Assignments `}
+        id="wd-course-assignments-link"
+      >
         Assignments
       </Link>
       <br />
-      <Link href="/Courses/1234/Quizzes" id="wd-course-Quizzes-link">
+      <Link href={`/Courses/${cid}/Quizzes`} id="wd-course-Quizzes-link">
         Quizzes
       </Link>
       <br />
-      <Link href="/Courses/1234/Grades" id="wd-course-Quizzes-link">
+      <Link href={`/Courses/${cid}/Grades`} id="wd-course-Quizzes-link">
         Grades
       </Link>
       <br />
-      <Link href="/Courses/1234/People" id="wd-course-People-link">
+      <Link href={`/Courses/${cid}/People`} id="wd-course-People-link">
         People
       </Link>
       <br />
--- app/Labs/layout.tsx
+++ app/Labs/layout.tsx
@@ -1,16 +1,14 @@
 import { ReactNode } from "react";
 import TOC from "./TOC";
-export default function LabsLayout({ children }: Readonly<{ children: ReactNode }>) {
-    return (
-        <table>
-            <tbody>
-                <tr>
-                    <td valign="top" width="100px">
-                        <TOC />
-                    </td>
-                    <td valign="top">{children}</td>
-                </tr>
-            </tbody>
-        </table>
-    );
+export default function LabsLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
+  return (
+    <div className="d-flex">
+      <div className="d-none d-md-block" style={{ width: "100px" }}>
+        <TOC />
+      </div>
+      <div className="flex-fill">{children}</div>
+    </div>
+  );
 }
