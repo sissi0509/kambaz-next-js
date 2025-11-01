dd1d0ceceb76926be7b9e3945d354803c93b8944
xi zhao <sissi.zhao09@gmail.com>
2025-11-01 19:33:14 -0400

    comment out eventobject.tsx

--- app/Labs/Lab4/EventObject.tsx
+++ app/Labs/Lab4/EventObject.tsx
@@ -1,24 +1,23 @@
 "use client";
 import { useState } from "react";
 export default function EventObject() {
-  const [event, setEvent] = useState<any>(null);
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    // e.target = e.target.outerHTML;
-    // delete e.view;
-    const obj = { ...e, target: (e.target as HTMLElement).outerHTML };
-    setEvent(obj);
-  };
+  // const [event, setEvent] = useState(null);
+  // const handleClick = (e: any) => {
+  //   e.target = e.target.outerHTML;
+  //   delete e.view;
+  //   setEvent(e);
+  // };
   return (
     <div>
       <h2>Event Object</h2>
       <button
-        onClick={(e) => handleClick(e)}
+        // onClick={(e) => handleClick(e)}
         className="btn btn-primary"
         id="wd-display-event-obj-click"
       >
         Display Event Object
       </button>
-      <pre>{JSON.stringify(event, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(event, null, 2)}</pre> */}
       <hr />
     </div>
   );
