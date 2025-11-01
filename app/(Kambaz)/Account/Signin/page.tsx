36220d9c1e765e37c6b11d11d238e5f501ae546f
xi zhao <sissi.zhao09@gmail.com>
2025-11-01 19:45:53 -0400

    haddle sign in

--- app/(Kambaz)/Account/Signin/page.tsx
+++ app/(Kambaz)/Account/Signin/page.tsx
@@ -1,29 +1,57 @@
+"use client";
 import Link from "next/link";
+import { redirect } from "next/dist/client/components/navigation";
+import { setCurrentUser } from "../reducer";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import * as db from "../../Database";
+import { FormControl } from "react-bootstrap";
+
 export default function Signin() {
+  const [credentials, setCredentials] = useState<any>({});
+  const dispatch = useDispatch();
+  const signin = () => {
+    const user = db.users.find(
+      (u) =>
+        u.username === credentials.username &&
+        u.password === credentials.password
+    );
+    if (!user) return;
+    dispatch(setCurrentUser(user));
+    redirect("/Dashboard");
+  };
+
   return (
     <div id="wd-signin-screen">
       <h1>Sign in</h1>
-      <input
+      <FormControl
+        defaultValue={credentials.username}
+        onChange={(e) =>
+          setCredentials({ ...credentials, username: e.target.value })
+        }
         placeholder="username"
         id="wd-username"
-        className="form-control mb-2"
-        defaultValue="alice"
+        className="mb-2"
       />
 
-      <input
+      <FormControl
+        defaultValue={credentials.password}
+        onChange={(e) =>
+          setCredentials({ ...credentials, password: e.target.value })
+        }
         placeholder="password"
         type="password"
         id="wd-password"
-        className="form-control mb-2"
+        className="mb-2"
       />
 
-      <Link
-        href="/Dashboard"
+      <button
+        onClick={signin}
         id="wd-signin-btn"
         className="btn btn-primary w-100 mb-2"
       >
         Sign in
-      </Link>
+      </button>
       <br />
       <Link href="Signup" id="wd-signup-link">
         Sign up
