61a74529924245f15abdb7e6fcb881de3af97bc6
xi zhao <sissi.zhao09@gmail.com>
2025-11-01 20:49:31 -0400

    dynamic account page

--- app/(Kambaz)/Account/Navigation.tsx
+++ app/(Kambaz)/Account/Navigation.tsx
@@ -1,30 +1,53 @@
+"use client";
 import Link from "next/link";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
+import { Nav, NavItem, NavLink } from "react-bootstrap";
+import { usePathname } from "next/navigation";
 export default function AccountNavigation() {
+  const { currentUser } = useSelector(
+    (state: RootState) => state.accountReducer
+  );
+  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const pathname = usePathname();
   return (
-    <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-      <Link
-        href="Signin"
-        id="wd-account-signin-link"
-        className="list-group-item active"
-      >
-        Signin
-      </Link>
+    <Nav variant="pills">
+      {links.map((link) => (
+        <NavItem key={link}>
+          <NavLink
+            as={Link}
+            href={link}
+            active={pathname.endsWith(link.toLowerCase())}
+          >
+            {link}
+          </NavLink>
+        </NavItem>
+      ))}
+    </Nav>
+    // <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
+    //   <Link
+    //     href="Signin"
+    //     id="wd-account-signin-link"
+    //     className="list-group-item active"
+    //   >
+    //     Signin
+    //   </Link>
 
-      <Link
-        href="Signup"
-        id="wd-account-signup-link"
-        className="list-group-item text-danger border-0"
-      >
-        Signup
-      </Link>
+    //   <Link
+    //     href="Signup"
+    //     id="wd-account-signup-link"
+    //     className="list-group-item text-danger border-0"
+    //   >
+    //     Signup
+    //   </Link>
 
-      <Link
-        href="Profile"
-        id="wd-account-profile-link"
-        className="list-group-item text-danger border-0"
-      >
-        Profile
-      </Link>
-    </div>
+    //   <Link
+    //     href="Profile"
+    //     id="wd-account-profile-link"
+    //     className="list-group-item text-danger border-0"
+    //   >
+    //     Profile
+    //   </Link>
+    // </div>
   );
 }
--- app/(Kambaz)/Account/page.tsx
+++ app/(Kambaz)/Account/page.tsx
@@ -1,4 +1,15 @@
+"use client";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
 import { redirect } from "next/dist/client/components/navigation";
 export default function AccountPage() {
+  const currentUser = useSelector(
+    (state: RootState) => state.accountReducer.currentUser
+  );
+
+  if (!currentUser) {
     redirect("/Account/Signin");
+  } else {
+    redirect("/Account/Profile");
+  }
 }
