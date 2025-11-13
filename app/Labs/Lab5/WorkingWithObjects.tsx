f20c712c979b9bc78b9044c56d254a2e126ffa17
xi zhao <sissi.zhao09@gmail.com>
2025-11-13 00:43:12 -0500

    finish lab

---
+++ app/Labs/Lab5/WorkingWithObjects.tsx
@@ -0,0 +1,137 @@
+"use client";
+import React, { useState } from "react";
+import { FormCheck, FormControl } from "react-bootstrap";
+const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
+export default function WorkingWithObjects() {
+  const [assignment, setAssignment] = useState({
+    id: 1,
+    title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-10-10",
+    completed: false,
+    score: 0,
+  });
+  const [module, setModule] = useState({
+    id: 1,
+    name: "Introduction to Web Development",
+    description: "Learn to build web applications",
+    course: "Web Development",
+  });
+
+  const ASSIGNMENT_API_URL = `${HTTP_SERVER}/lab5/assignment`;
+  const MODULE_API_URL = `${HTTP_SERVER}/lab5/module`;
+
+  return (
+    <div id="wd-working-with-objects">
+      <h3>Working With Objects</h3>
+      <h4>Working with Assignment</h4>
+      <h4>Modifying Properties</h4>
+      <a
+        id="wd-update-assignment-title"
+        className="btn btn-primary float-end"
+        href={`${ASSIGNMENT_API_URL}/title/${assignment.title}`}
+      >
+        Update Title
+      </a>
+      <FormControl
+        className="w-75"
+        id="wd-assignment-title"
+        defaultValue={assignment.title}
+        onChange={(e) =>
+          setAssignment({ ...assignment, title: e.target.value })
+        }
+      />
+      <hr />
+      <a
+        id="wd-update-assignment-score"
+        className="btn btn-primary float-end"
+        href={`${ASSIGNMENT_API_URL}/score/${assignment.score}`}
+      >
+        Update score
+      </a>
+      <FormControl
+        className="w-75"
+        id="wd-assignment-score"
+        type="number"
+        defaultValue={assignment.score}
+        onChange={(e) =>
+          setAssignment({ ...assignment, score: Number(e.target.value) })
+        }
+      />
+      <hr />
+      <a
+        id="wd-update-assignment-completed"
+        className="btn btn-primary float-end"
+        href={`${ASSIGNMENT_API_URL}/completed/${assignment.completed}`}
+      >
+        Update completed
+      </a>
+      <FormCheck
+        id="wd-assignment-completed"
+        type="checkbox"
+        label="Completed"
+        checked={assignment.completed}
+        onChange={(e) =>
+          setAssignment({ ...assignment, completed: e.target.checked })
+        }
+      />
+      <hr />
+      <hr />
+      <h4>Retrieving Properties</h4>
+      <a
+        id="wd-retrieve-assignment-title"
+        className="btn btn-primary"
+        href={`${HTTP_SERVER}/lab5/assignment/title`}
+      >
+        Get Title
+      </a>
+      <hr />
+
+      <h4>Retrieving Objects</h4>
+      <a
+        id="wd-retrieve-assignments"
+        className="btn btn-primary"
+        href={`${HTTP_SERVER}/lab5/assignment`}
+      >
+        Get Assignment
+      </a>
+      <hr />
+      <h4>Working with Module</h4>
+
+      <h4>Updata module name</h4>
+      <a
+        id="wd-update-module-name"
+        className="btn btn-primary float-end"
+        href={`${MODULE_API_URL}/name/${module.name}`}
+      >
+        Update Name
+      </a>
+      <FormControl
+        className="w-75"
+        id="wd-module-name"
+        defaultValue={module.name}
+        onChange={(e) => setModule({ ...module, name: e.target.value })}
+      />
+      <hr />
+
+      <h4>Retrieve Module Object</h4>
+      <a
+        id="wd-retrieve-module"
+        className="btn btn-primary"
+        href={MODULE_API_URL}
+      >
+        Get Module
+      </a>
+      <hr />
+      <h4>Retrieve Module Name</h4>
+      <a
+        id="wd-retrieve-module"
+        className="btn btn-primary"
+        href={`${MODULE_API_URL}/name`}
+      >
+        Get Module Name
+      </a>
+      <hr />
+    </div>
+  );
+}
