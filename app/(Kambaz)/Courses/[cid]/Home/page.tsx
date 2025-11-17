f0586c2e73ab9425787eabc6959ae1b2527b42b9
xi zhao <sissi.zhao09@gmail.com>
2025-11-16 23:40:32 -0500

    delete check enrol in course

--- app/(Kambaz)/Courses/[cid]/Home/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Home/page.tsx
@@ -1,32 +1,18 @@
 "use client";
 import Modules from "../Modules/page";
 import CourseStatus from "./Status";
-import { redirect, useParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 export default function Home() {
-  const { cid } = useParams();
-
+  const router = useRouter();
   const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const enrollments = useSelector(
-    (state: any) => state.enrollmentsReducer.enrollments
-  );
-
-  const isEnrolled = enrollments.some(
-    (e: any) => e.user === currentUser?._id && e.course === cid
-  );
 
   useEffect(() => {
     if (!currentUser) {
-      redirect("/Account/Signin");
-      return;
+      router.replace("/Account/Signin");
     }
-
-    if (!isEnrolled) {
-      redirect("/Dashboard");
-      return;
-    }
-  }, [currentUser, isEnrolled, cid]);
+  }, [currentUser, router]);
 
   return (
     <div className="d-flex" id="wd-home">
